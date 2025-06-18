9c8bf9c081833819b650d89ef38bb1b0e1828226
Arshdeep <harkiratkaur8c6@gmail.com>
2025-06-18 21:09:37 +0530

    Done listing out all places from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listings.js");
 
 main()
 .then( (result) => {
@@ -13,6 +14,12 @@ main()
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/airBnb');
 }
+
+app.get("/listings", async (req, res) => {
+  const Listings = await Listing.find();
+  console.log(Listings);
+  res.render("mainPage.ejs", {Listings});
+});
 
 app.listen(8080, () => {
     console.log("Started Listening At port 8080");
