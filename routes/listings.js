e3d9d13d1bb0f5ba36f7593cdaa12a3df988a2c8
Arshdeep <harkiratkaur8c6@gmail.com>
2025-07-23 09:11:21 +0530

    Used router.route for routing in one, different responses set for same request route

--- routes/listings.js
+++ routes/listings.js
@@ -17,26 +17,20 @@ const validateListing = (req, res, next) => {
   next();
 };
 
-// REQUEST FOR LISTING DOWN ALL LOCATIONS IN DATABASE
-router.get("/", wrapAsync(listingController.index));
-
+//router.route is used to pair up response routes based upon their request path
+router.route("/")
+.get( wrapAsync(listingController.index) ) // REQUEST FOR LISTING DOWN ALL LOCATIONS IN DATABASE
+.post( validateListing, wrapAsync(listingController.add) ) // REQUEST FOR ADDING NEW LOCATION IN DATABASE AND REDIRECTING AFTER THIS 
 
 // REQUEST FOR PROVIDING A FORM FOR ADDING NEW LOCATION
 router.get("/new",  LoggedIn, listingController.new);
 
-// REQUEST FOR ADDING NEW LOCATION IN DATABASE AND REDIRECTING AFTER THIS 
-router.post("/", validateListing, wrapAsync(listingController.add));
-
-// REQUEST FOR SHOWING PARTICULAR LISTING DETAIL ON CLICKING IT
-router.get("/:id", wrapAsync(listingController.show));
+router.route("/:id")
+.get( wrapAsync(listingController.show) ) // REQUEST FOR SHOWING PARTICULAR LISTING DETAIL ON CLICKING IT
+.put( isOwner, wrapAsync(listingController.editAndShow) ) // REQUEST FOR REDIRECTING TO SHOW.EJS PAGE AFTER EDITING
+.delete( LoggedIn, wrapAsync(listingController.destroy) ) // REQUEST FOR DELETING A PARTICULAR LOCATION
 
 // REQUEST FOR PROVIDING FORM FOR EDITING LISTING DETAILS
 router.get("/:id/edit", LoggedIn, wrapAsync(listingController.edit));
-
-// REQUEST FOR REDIRECTING TO SHOW.EJS PAGE AFTER EDITING
-router.put("/:id", isOwner, wrapAsync(listingController.editAndShow));
-
-// REQUEST FOR DELETING A PARTICULAR LOCATION
-router.delete("/:id", LoggedIn, wrapAsync(listingController.destroy));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,17 +7,13 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
-// Route for sending out form when user wants to sign-up
-router.get("/signup", userController.signupForm);
+router.route("/signup")
+.get(userController.signupForm)  // Route for sending out form when user wants to sign-up
+.post(wrapAsync( userController.signup )); //Route for saving user data in database and redirecting to listings page
 
-//Route for saving user data in database and redirecting to listings page
-router.post("/signup", wrapAsync( userController.signup ));
-
-//Route for providing a form to Login
-router.get("/login", userController.loginForm);
-
-// For passport.authenticate middleware to work your form fields name must be like username, password not like user[username], user[password]
-router.post("/login", saveRedirectUrl, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),  wrapAsync( userController.login ));
+router.route("/login")
+.get(userController.loginForm) //Route for providing a form to Login
+.post(saveRedirectUrl, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),  wrapAsync( userController.login )); // For passport.authenticate middleware to work your form fields name must be like username, password not like user[username], user[password]
 
 //Route for to logout the user as soon as this request comes
 router.get("/logout", userController.logout);
